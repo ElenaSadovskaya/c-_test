20949156804916fa85da287f36ef21133d834e76
ElenaSadovskaya <elena_93@list.ru>
2019-10-06 20:00:59 +0300

    Задание 15

--- Education_web_test/Education_web_test/Tests/Contact/ContactCreationTests.cs
+++ Education_web_test/Education_web_test/Tests/Contact/ContactCreationTests.cs
@@ -4,6 +4,10 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace Education_web_test
 {
@@ -25,11 +29,35 @@ namespace Education_web_test
             return contact;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromFileFromCSV()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0], parts[1], parts[2]));
+            }
+            return contacts;
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromFileFromXML()
+        {
+
+            return (List<ContactData>)
+                new XmlSerializer(typeof(List<ContactData>)).Deserialize(new StreamReader(@"contacts.xml"));
+        }
+
+        public static IEnumerable<ContactData> ContactDataFromFileFromJSON()
+        {
+            return JsonConvert.DeserializeObject<List<ContactData>>(File.ReadAllText(@"contacts.json"));
+        }
+
+        [Test, TestCaseSource("ContactDataFromFileFromJSON")]
 
         public void ContactCreation(ContactData contact)
         {
-                        List<ContactData> oldContact = app.Contact.GetContactList();
+            List<ContactData> oldContact = app.Contact.GetContactList();
             app.Contact.Create(contact);
             app.Navigation.OpenContacts();
             List<ContactData> newContact = app.Contact.GetContactList();
